282c0a655207c81e3547bc12cf057e98b2363139
eliraybon <elr0615@gmail.com>
2019-12-16 16:14:07 -0500

    Add user queries

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -5,3 +5,30 @@ export const IS_LOGGED_IN = gql`
     isLoggedIn @client
   }
 `;
+
+export const FETCH_USERS = gql`
+  query fetchUsers {
+    users {
+      _id
+      name
+
+    }
+  }
+`;
+
+export const FETCH_USER = gql`
+  query fetchUser($id: ID!) {
+    user(_id: $id) {
+      _id
+      name
+      owner
+      favoriteStores {
+        name
+      }
+      favoriteProducts {
+        name
+        price 
+      }
+    }
+  }
+`;
