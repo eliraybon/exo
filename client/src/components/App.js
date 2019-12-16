11108f4c861281b759a3ce27791e9e44dd18845d
M1CH43L <michaellsimeon@gmail.com>
2019-12-16 16:57:05 -0500

    Fix logout function

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import AuthRoute from '../util/route_util';
@@ -8,11 +8,13 @@ import Nav from './ui/Nav';
 const App = () => {
   return (
     <div>
-      <Switch>
-        <AuthRoute exact path="/login" component={Login} routeType="auth" />
-        <AuthRoute exact path="/register" component={Register} routeType="auth" />
-        <Route path="/" component={Nav} />
-      </Switch>
+      <HashRouter>
+        <AuthRoute path="/" component={Nav} routeType="" />
+        <Switch>
+          <AuthRoute exact path="/login" component={Login} routeType="auth" />
+          <AuthRoute exact path="/register" component={Register} routeType="auth" />
+        </Switch>
+      </HashRouter>
     </div>
   );
 };
