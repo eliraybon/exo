84fca255c31fdd5ade3839f91f55296a6b94fc4d
eliraybon <elr0615@gmail.com>
2019-12-16 22:03:50 -0500

    Refactor to add products to user pages

---
+++ client/src/components/products/ProductIndexItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class ProductIndexItem extends React.Component {
+  render() {
+    const { product } = this.props;
+    return (
+      <li 
+        onClick={() => this.props.history.push(`/products/${product._id}`)}
+        className="pi-product-detail"
+        key={product._id}
+      >
+        <div 
+          className="pi-product-image" 
+          style={{ backgroundImage: `url(${product.image})` }}>
+        </div>
+
+        <div className="pi-price">${product.price}</div>
+      </li>
+    )
+  }
+}
+
+export default withRouter(ProductIndexItem);
