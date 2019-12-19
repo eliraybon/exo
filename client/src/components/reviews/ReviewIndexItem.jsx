1a4952a9168b244cfad7a04e00b368164ea5bbcf
eliraybon <elr0615@gmail.com>
2019-12-19 12:31:23 -0500

    Implement review delete

--- client/src/components/reviews/ReviewIndexItem.jsx
+++ client/src/components/reviews/ReviewIndexItem.jsx
@@ -1,12 +1,65 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+
+import { CURRENT_USER, FETCH_PRODUCT } from '../../graphql/queries';
+import { DELETE_REVIEW, UPDATE_REVIEW } from '../../graphql/mutations';
+
 
 export default class ReviewIndexItem extends React.Component {
+  handleDeleteReview = (e, deleteReview) => {
+    e.preventDefault();
+    deleteReview({
+      variables: {
+        reviewId: this.props.review._id
+      }
+    })
+  }
+
   render() {
     const { review } = this.props;
     return (
       <li>
         {review.rating}
         {review.body}
+
+        <Query query={CURRENT_USER}>
+          {({ loading, error, data }) => {
+            if (loading) return null;
+            if (error) return <p>Error</p>
+            const { currentUser } = data;
+
+            if (review.author._id === currentUser) {
+              return (
+                <div className="rii-update-and-delete">
+                  <Mutation 
+                    mutation={DELETE_REVIEW}
+                    refetchQueries={[
+                      {
+                        query: FETCH_PRODUCT,
+                        variables: {
+                          _id: this.props.review.product._id
+                        }
+                      }
+                    ]}
+                  >
+                    {(deleteReview => {
+                      return (
+                        <button 
+                          onClick={e => this.handleDeleteReview(e, deleteReview)}
+                          className="rii-delete-button"
+                        >
+                          Delete
+                        </button>
+                      )
+                    })}
+                  </Mutation>
+                </div>
+              )
+            } else {
+              return null;
+            }
+          }}
+        </Query>
       </li>
     )
   }
