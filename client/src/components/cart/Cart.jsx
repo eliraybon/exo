ebad97cfc9175b5a8d6bcf476d2cf60fb3be2cd3
js4484 <js4484@columbia.edu>
2019-12-20 12:58:01 -0500

    Style cart page

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -9,24 +9,23 @@ class Cart extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { currentUser: null, loading: true }
+    this.state = { currentUser: null, loading: true, prices: [] }
   }
 
+  priceTotal(cart) {
+
+  }
 
   render() {
     return (
-
       <ApolloConsumer>
         {client => {
           // debugger
           client.mutate({ mutation: VERIFY_USER, variables: { token: localStorage.getItem("auth-token") } })
             .then(({ data: verifyUser }) => {
-              
               this.setState({ loading: false, currentUser: verifyUser.verifyUser._id })
             })
-
           if (this.state.loading) return <div>Loading...</div>
-          
           return(
             <Query query={FETCH_USER_CART} variables={{ id: this.state.currentUser }}>
               {({ loading, error, data }) => {
@@ -34,13 +33,71 @@ class Cart extends React.Component {
                 if (error) return <p>Error</p>;
                 // console.log(data)
 
+                if (data.user.cartProducts.length < 1) {
+                  return (
+                    <div className="cart-outer to-flex">
+                      <div className="cart-ribbon to-flex-col col-center">
+                        <div className="cart-big-box cart-box-empty">
+                          <div className="cart-empty-above">Your cart is empty</div>
+                          <div className="cart-empty-linebreak"></div>
+                          <div className="cart-empty-below" onClick={() => this.props.history.push(`/products/`)}>Discover something amazing to fill it up</div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  )
+                }
                 return (
-                  <div>
-                    {data.user.cartProducts.map(product => {
-                      return (
-                        <ProductIndexItem key={product._id} product={product} />
-                      )
-                    })}
+                  <div className="cart-outer to-flex">
+                    <div className="cart-ribbon to-flex-col">
+                      <div className="cart-text-above to-flex">
+                        <div>{data.user.cartProducts.length} items in your cart</div>
+                        <div className="keep-shopping" onClick={() => this.props.history.push(`/products/`)}>keep shopping</div>
+                      </div>
+                      <div className="cart-big-box to-flex">
+                        <div className="cart-left-section to-flex-col">
+                          <ul>
+                            {data.user.cartProducts.map(product => {
+                              return (
+                                <ProductIndexItem key={product._id} product={product} cart={true} userId={this.state.currentUser}/>
+                              )
+                            })}
+                          </ul>
+                        </div>
+                        <div className="cart-right-section to-flex-col">
+                            <div className="cart-ditty">Thank you for your patronage! Please proceed to automatically charge your HoloCard account</div>
+                            <div className="cart-right-douple">
+                              <div className="cart-item-total">item total</div>
+                            <div className="cart-item-total">{}</div>
+                            </div>
+                            <div className="cart-right-douple">
+                              <div className="cart-discount">discount</div>
+                              <div className="cart-discount">discount</div>
+                            </div>
+                            
+                            <div className="cart-linebreak"></div>
+
+                            <div className="cart-right-douple">
+                              <div className="cart-subtotal">subtotal</div>
+                              <div className="cart-subtotal">subtotal</div>
+                            </div>
+
+                            <div className="cart-right-douple">
+                              <div className="cart-shipping">shipping</div>
+                              <div className="cart-shipping">hipping</div>
+                            </div>
+                            <div className="cart-linebreak"></div>
+                            <div className="cart-right-douple">
+                              <div className="cart-total">total</div>
+                              <div className="cart-total">total</div>
+                            </div>
+                            <button className="cart-button ps-cart-button">
+                              <div className="ps-button-text">Purchase</div><i className="far fa-hand-point-up"></i>
+                            </button>
+                        </div>
+                      </div>
+                      <div>text below</div>
+                    </div>
                   </div>
                 )
               }}
--- client/src/components/products/ProductIndexItem.jsx
+++ client/src/components/products/ProductIndexItem.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { REMOVE_FROM_CART } from "../../graphql/mutations";
+import { FETCH_USER_CART } from "../../graphql/queries";
+import { ApolloConsumer } from "react-apollo";
+
 
 class ProductIndexItem extends React.Component {
   render() {
-    const { product } = this.props;
+    const { product, cart, userId } = this.props;
+
+    if (cart) {
+      return (
+        <li className="cart-element-box to-flex">
+          <div 
+            className="cart-image"
+            style={{ backgroundImage: `url(${product.image})` }}
+            onClick={() => this.props.history.push(`/products/${product._id}`)}
+            >
+          </div>
+          <div className="to-flex large-douple">
+            <div className="cart-name to-flex-col">
+              <div className="cart-box-name">{product.name}</div>
+              <div className="cart-douple to-flex">
+                <div className="cart-btn">save</div>
+                <ApolloConsumer>
+                  {client => {
+                    return <div 
+                    className="cart-btn cart-remove" 
+                    onClick={() => client.mutate({ 
+                        mutation: REMOVE_FROM_CART, 
+                        variables: { userId: userId, productId: product._id },
+                        refetchQueries: [
+                          {
+                            query: FETCH_USER_CART,
+                            variables: {
+                              id: userId
+                            }
+                          }
+                        ]
+                       })}
+                    >
+                      remove
+                    </div>;
+                  }}
+                </ApolloConsumer>
+              </div>
+            </div>
+            <div className="cart-prod-price">${product.price}</div>
+          </div>
+        </li>
+      )
+    }
+
     return (
       <li 
         onClick={() => this.props.history.push(`/products/${product._id}`)}
