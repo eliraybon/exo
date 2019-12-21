e9d6aaf1b92685978aa3af60deaad71b29a3d739
eliraybon <elr0615@gmail.com>
2019-12-20 19:58:45 -0500

    Update heroku config

--- server/server.js
+++ server/server.js
@@ -30,9 +30,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
