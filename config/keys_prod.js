dd51b946fb23c0b4b1a65b1f3dbaed0dd51779c0
eliraybon <elr0615@gmail.com>
2019-12-20 20:51:36 -0500

    Fix heroku

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  AWSKey: process.env.AWSKey,
-  NASAKey: process.env.NASAKey,
-  foodKey: process.env.foodKey
+  AWSKey: process.env.AWS_KEY,
+  NASAKey: process.env.NASA_KEY,
+  foodKey: process.env.FOOD_KEY
 }
