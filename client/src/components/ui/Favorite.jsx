8b2ecdb3db7aa6b4d301559f79846d2406b67e82
js4484 <js4484@columbia.edu>
2019-12-30 06:42:32 -0500

    Style product favorite button

--- client/src/components/ui/Favorite.jsx
+++ client/src/components/ui/Favorite.jsx
@@ -103,7 +103,7 @@ class Favorite extends React.Component {
               className="favorite-button"
               onClick={e => this.handleAddProduct(e, addFavoriteProduct)}
             >
-              Add to Favorites
+                <i className="far fa-heart unfavorited"></i>
             </button>
           )}
           </Mutation>
@@ -155,7 +155,7 @@ class Favorite extends React.Component {
                 className="favorite-button"
                 onClick={e => this.handleDeleteProduct(e, deleteFavoriteProduct)}
               >
-                Remove from Favorites
+                <i className="far fa-heart favorited"></i>
             </button>
             )}
           </Mutation>
