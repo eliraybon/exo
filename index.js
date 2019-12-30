1a4493be67d4011b3004aeefa0e5b358a18aa146
js4484 <js4484@columbia.edu>
2019-12-30 12:07:14 -0500

    Prep for heroku

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = require("./server/server");
+const path = require('path');
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
