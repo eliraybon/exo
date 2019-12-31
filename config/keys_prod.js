df209e59ee58dd0fe7af42c271bbfebad99bb456
js4484 <js4484@columbia.edu>
2019-12-30 22:16:45 -0500

    Heroku fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -3,5 +3,7 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
   AWSKey: process.env.AWS_KEY,
   NASAKey: process.env.NASA_KEY,
-  foodKey: process.env.FOOD_KEY
+  foodKey: process.env.FOOD_KEY,
+  secretOrPrivateKey: process.env.SECRET_OR_PRIVATE_KEY
+
 }
